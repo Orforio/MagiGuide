339fceebe0c71f0eb225d0d2081ca4d2890d74c1
Richard Whittaker <pkeruno@gmail.com>
2018-08-02 00:11:06 +0100

    Initial configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		NgbModule.forRoot(),
+		ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
