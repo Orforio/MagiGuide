49e0f884a8bc0b647a46d7f1e511f3bd53d69450
Richard Whittaker <pkeruno@gmail.com>
2018-09-17 00:44:49 +0100

    Convert Fastpasses to feature module (#32)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { environment } from '../environments/environment';
-import { FastpassesComponent } from './fastpasses/fastpasses.component';
-import { FastpassComponent } from './fastpasses/fastpass/fastpass.component';
-import { AddFastpassComponent } from './fastpasses/add-fastpass/add-fastpass.component';
 
 @NgModule({
+	bootstrap: [ AppComponent ],
 	declarations: [
 		AppComponent,
-		DashboardComponent,
-		FastpassComponent,
-		FastpassesComponent,
-		AddFastpassComponent
+		DashboardComponent
 	],
 	imports: [
 		AppRoutingModule,
 		BrowserModule,
-		FormsModule,
 		NgbModule,
 		ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
 	],
-	providers: [],
-	bootstrap: [ AppComponent ]
+	providers: []
 })
 export class AppModule { }
