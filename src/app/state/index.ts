a1acbd5969020fc717db17534901f0f1db09bfaa
Richard Whittaker <pkeruno@gmail.com>
2019-02-17 03:00:21 +0000

    Install NgrxStoreLocalstorage

--- src/app/state/index.ts
+++ src/app/state/index.ts
@@ -1,5 +1,7 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { storeFreeze } from 'ngrx-store-freeze';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
 
 import { environment } from '../../environments/environment';
 
@@ -11,4 +13,14 @@ export const reducers: ActionReducerMap<State> = {
 	temp: null
 };
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [storeFreeze] : [];
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+	return localStorageSync({
+		keys: ['fastpasses'],
+		rehydrate: true
+	})(reducer);
+}
+
+export const metaReducers: MetaReducer<State>[] = [
+	localStorageSyncReducer,
+	!environment.production ? storeFreeze : null
+];
