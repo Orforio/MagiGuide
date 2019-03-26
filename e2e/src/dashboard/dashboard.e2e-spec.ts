d3b05bb26945b3f44c9f19642c74e1f65e2a4ebc
Richard Whittaker <pkeruno@gmail.com>
2019-03-26 22:14:58 +0000

    Make Fastpass clickable

--- e2e/src/dashboard/dashboard.e2e-spec.ts
+++ e2e/src/dashboard/dashboard.e2e-spec.ts
@@ -1,13 +1,16 @@
 import { browser } from 'protractor';
 
 import { DashboardPage } from './dashboard.po';
+import { FastpassesPage } from '../fastpasses/fastpasses.po';
 
 describe('Dashboard Page', () => {
 	let dashboardPage: DashboardPage;
+	let fastpassesPage: FastpassesPage;
 
 	beforeEach(() => {
 		// Arrange
 		dashboardPage = new DashboardPage();
+		fastpassesPage = new FastpassesPage();
 
 		// Act
 		dashboardPage.navigateToDashboard();
@@ -20,6 +23,19 @@ describe('Dashboard Page', () => {
 	it('should display the Dashboard header', () => {
 		// Assert
 		expect(dashboardPage.getPageTitle()).toEqual('MagiGuide Dashboard');
+	});
+
+	it('should navigate to the Fastpasses page when the Fastpass is clicked', () => {
+		// Arrange
+		fastpassesPage.navigateToFastpasses();
+		fastpassesPage.fillInAddFastpass('Star Tours', [23, 30], [23, 55], [23, 55]);
+		dashboardPage.navigateToDashboard();
+
+		// Act
+		dashboardPage.getNextFastpass().click();
+
+		// Assert
+		expect(browser.getCurrentUrl()).toContain('/fastpasses');
 	});
 
 	it('should navigate to the Fastpasses page when "Add new Fastpasses" is clicked', () => {
